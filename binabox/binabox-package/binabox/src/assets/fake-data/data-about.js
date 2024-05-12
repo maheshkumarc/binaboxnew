c72212fbc1f4dde6ed0b73d5273bd49b543414da
unknown <mahesh143mal@gmail.com>
2024-05-12 22:34:52 +0530

    first commit

---
+++ binabox/binabox-package/binabox/src/assets/fake-data/data-about.js
@@ -0,0 +1,46 @@
+const dataAbout = [
+[
+    {
+        id: 1,
+        title : 'Rug Pulls & Honeypotssss',
+        text : 'Investors pour money into a project that vanishes overnight, leaving them with worthless tokens. A staggering $2.8 billion worth of investor funds were lost to rug pulls in 2021 alone (Source: CNBC)'
+    },
+    {
+        id: 2,
+        title : 'Whale Manipulation',
+        text : 'Whale Manipulationvestors (whales) can manipulate prices through massive buys and sells, leading to unpredictable market swings and hefty losses for smaller investors.'
+    },
+    {
+        id: 3,
+        title : 'FUD (Fear, Uncertainty, Doubt)',
+        text : 'Negative rumors can be easily spread online, causing panic selling and driving down prices, even for legitimate projects.'
+    },
+    {
+        id: 4,
+        title : 'Gauging Project Legitimacy',
+        text : 'Identifying truly innovative projects with strong potential from fly-by-night scams can be an overwhelming task.'
+    },
+],
+[
+    {
+        id: 1,
+        title : 'Rug Pulls & Honeypots Detection',
+        text : 'Our advanced AI analyzes on-chain activity to identify suspicious patterns including analyzing smart contract code, liquidity pool movements, and developer activity to flag potential scams before you invest'
+    },
+    {
+        id: 2,
+        title : 'Whale Tracking & Price Movement Anticipation',
+        text : 'Radar AI tracks the movements of large investors (whales) and analyzes their trading patterns. This allows you to anticipate potential price swings caused by whale activity and make informed investment decisions before the market reacts.'
+    },
+    {
+        id: 3,
+        title : 'On-chain & Twitter Sentiment Analysis',
+        text : 'Radar AI goes beyond the blockchain by analyzing on-chain transaction volume to gauge investor sentiment towards specific projects and the overall market. Additionally, we integrate Twitter sentiment analysis to provide a holistic view of public perception. This valuable insight helps you understand the current mood and make informed investment decisions based on more than just price charts.'
+    },
+   
+]
+]
+
+
+
+export default dataAbout;
